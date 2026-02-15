cd68a971b18173a8554959f84755f43d916e81cb
josuenataren23 <josue.nataren23@unach.mx>
2026-02-15 16:30:11 -0600

    Agregar archivos de proyecto.

---
+++ Gestor.Finanzas/Controllers/FuncionesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gestor.Finanzas.Controllers
+{
+    public class FuncionesController : Controller
+    {
+        // GET: Funciones
+        public ActionResult Funciones()
+        {
+            return View();
+        }
+    }
+}
---
+++ Gestor.Finanzas/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./Views/**/*.cshtml",
+        "./Scripts/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
+
