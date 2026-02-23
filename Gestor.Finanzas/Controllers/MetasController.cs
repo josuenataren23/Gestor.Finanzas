91d9e4d79f168880b95abb7f7f13573b8992fd7d
josuenataren23 <josue.nataren23@unach.mx>
2026-02-23 08:26:29 -0600

    Se agrego seccion Metas del dashboard

---
+++ Gestor.Finanzas/Controllers/MetasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gestor.Finanzas.Controllers
+{
+    public class MetasController : Controller
+    {
+        // GET: Metas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
